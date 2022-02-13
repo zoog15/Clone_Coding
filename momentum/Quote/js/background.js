eef620377cf4b85bda7e1eacde3f705032c78bd7
KimMinKwan <zoog15@naver.com>
2022-02-13 17:27:52 +0900

    TIL Update

---
+++ momentum/Quote/js/background.js
@@ -0,0 +1,10 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+// body에 html을 추가함
+document.body.appendChild(bgImage);
